95ab618f8a175143489fb23bbd016597f402caa8
jborjaCeiba <jorge.borja@ceiba.com.co>
2024-11-29 08:19:51 -0500

    Ajuste de las pruebas

--- test/Pages/generatorPage_test.dart
+++ test/Pages/generatorPage_test.dart
@@ -11,12 +11,27 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pocmirror/main.dart';
 
 void main() {
-  testWidgets('Validar Generador', (WidgetTester tester) async {
+  testWidgets('Validar Boton Like', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('Like 2'), findsOneWidget);
+
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+
+    // // Verify that our counter has incremented.
+    // expect(find.text('Home'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('Validar Boton Next', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
     expect(find.text('Next'), findsOneWidget);
 
     // // Tap the '+' icon and trigger a frame.
